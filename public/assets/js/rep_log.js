fc2b0bf4002e5dfe7038d2bb21d689da2aa82349
Max <ageev.maks2014@gmail.com>
2019-02-25 17:18:46 +0500

    4. Component Organization

---
+++ public/assets/js/rep_log.js
@@ -0,0 +1,7 @@
+'use strict';
+const $ = require('jquery');
+const RepLogApp = require('./Componets/RepLogApp');
+
+
+var $wrapper = $('.js-rep-log-table');
+var repLogApp = new RepLogApp($wrapper);
